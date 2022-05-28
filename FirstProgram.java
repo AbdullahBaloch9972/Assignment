9d2c78f40b25c856208b4b43568da368030447e8
AbdullahBaloch9972 <106428693+AbdullahBaloch9972@users.noreply.github.com>
2022-05-28 14:32:20 +0500

    My First Program in java

---
+++ FirstProgram.java
@@ -0,0 +1,10 @@
+public class FirstProgram {
+
+   /* Hi Sir I am Abdullah 2k21
+    * This is my First Program
+    */
+
+   public static void main(String []args) {
+      System.out.println("Hello World"); 
+   }
+}
